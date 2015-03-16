c6f249ebc448e1fe59a7ff2d418f4599ed28eb8a
Buck Young <buckcyoung@gmail.com>
2015-03-16 01:18:31 -0400

    add representations

--- NameGenerator.java
+++ NameGenerator.java
@@ -70,35 +70,35 @@ public class NameGenerator {
 	};
 
 	private static final String[] lastNames = {
-		"Cheddarwood", 
-		"Wallsqueaker", 
-		"Squeakswell", 
-		"Swisser", 
-		"Mazerunner", 
-		"Wheelrunner", 
-		"Pelleteater", 
-		"Chompsky", 
-		"Skeeter", 
-		"Anderson",
-		"Whiskers",
-		"Zamyatin",
-		"Archer",
-		"Figgis",
-		"Woodhouse",
-		"Tunt",
-		"Kane",
-		"Belcher",
-		"Krieger",
-		"Trexler",
-		"Beeblebrox",
-		"Dallas",
-		"Zeus",
-		"Feynman",
-		"Hagen",
-		"Chah"
-		// "\u2764", //heart
-		// "\u262F", //yinyang
-		// "\u2744", //snowflake
+		// "Cheddarwood", 
+		// "Wallsqueaker", 
+		// "Squeakswell", 
+		// "Swisser", 
+		// "Mazerunner", 
+		// "Wheelrunner", 
+		// "Pelleteater", 
+		// "Chompsky", 
+		// "Skeeter", 
+		// "Anderson",
+		// "Whiskers",
+		// "Zamyatin",
+		// "Archer",
+		// "Figgis",
+		// "Woodhouse",
+		// "Tunt",
+		// "Kane",
+		// "Belcher",
+		// "Krieger",
+		// "Trexler",
+		// "Beeblebrox",
+		// "Dallas",
+		// "Zeus",
+		// "Feynman",
+		// "Hagen",
+		// "Chah"
+		"\u2764", //heart
+		"\u262F", //yinyang
+		"\u2744", //snowflake
 	};
 
 	public static String generateFirstName(Gender gender) {
